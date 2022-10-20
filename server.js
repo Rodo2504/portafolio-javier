49f5d63f9f72a31003168afd7b885a21b277a567
unknown <rodolfoq60@gmail.com>
2022-10-20 12:14:24 -0500

    cambios en el server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express   = require('express');
 const path      = require('path');
 const app       = express();
 
-app.use(express.static('./dist/app'));
+app.use(express.static('./dist/portafolio-javier'));
 
 app.get('/*', function(req, res){
-	res.sendFile(path.join(__dirname, '/dist/app/index.html'));
+	res.sendFile(path.join(__dirname, '/dist/portafolio-javier/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
